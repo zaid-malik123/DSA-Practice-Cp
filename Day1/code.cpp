6c7d316a8c85f9365a6f5cf6f6883463e0a49858
Zaid Malik <zaid malik>
2025-07-30 01:04:35 +0530

    day1

---
+++ Day1/code.cpp
@@ -0,0 +1,66 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     string name;
+//     cout << "what's yout name ??";
+//     cin >> name;
+//     cout << "hello" << name;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a, b, sum;
+//     cout << "Enter the a digit number";
+//     cin>> a;
+//     cout<< "Enter the b digit number";
+//     cin>> b;
+//     sum = a+b;
+//     cout<< "tumhare total hisab banta hai sir ji " << sum;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string tell;
+//     cout << "tell me anything ";
+//     cin>>tell;
+//     cout<< "you told me  "<<tell;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"chal tu ek number likh: ";
+//     cin>>a;
+//     cout<<"jo tune number likha tha vo hai "<<a;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float a;
+//     cout<< "abke bar tu ek poin wala number de ";
+//     cin>>a;
+//     cout<<"jo tune point wala number diya tha "<<a;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a, b, diff;
+//     cout<<"ek number bta koi bhi ";
+//     cin>> a;
+//     cout<<"ek aur bta koi bhi number";
+//     cin>>b;
+//     diff = a - b;
+//     cout<< "bhai tere number ke beech ka diffrence hai "<<diff;
+//     return 0;
+// }
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DSA-Practice-Cp
 My DSA journey using cp
+
+# 🚀 Day 1 – C++ Basics Practice
+
+Welcome to **Day 1** of my **C++ DSA Practice Journey**!  
+Today, I focused on getting comfortable with C++ fundamentals by writing a few basic programs.
+
+This day was all about understanding how to:
+- Take input from users using `cin`
+- Display output with `cout`
+- Work with basic data types like `int`, `float`, and `string`
+- Perform simple operations like addition and subtraction
+
+---
+
+## ✅ Programs Practiced:
+
+1.  Take User Name as Input
+2. ➕ Add Two Integers
+3. 💬 Echo What User Types
+4. 🔢 Echo User Number
+5. 🌡️ Input a Float Number
+6. ➖ Subtract Two Integers
