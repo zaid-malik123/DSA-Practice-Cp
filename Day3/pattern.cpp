80ffefa28e394de5663de38aab3b44cc36812a8b
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-02 00:41:10 +0530

    pattern-practice

---
+++ Day3/pattern.cpp
@@ -0,0 +1,169 @@
+ // ques 1:-   1 2 3 4
+            // 1 2 3 4
+            // 1 2 3 4
+            // 1 2 3 4
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<< "Enter the number: ";
+// cin>>n;
+// for(int i = 1; i<=n; i++){
+//     for(int j = 1; j <= n; j++){
+//         cout<<j << " ";
+//     }
+//     cout<<endl;
+// }
+
+//     return 0;
+// }
+
+// ques 2:- A B C D
+        //  A B C D
+        //  A B C D
+        //  A B C D
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<< "Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         char ch = 'A';
+//         for(int j = 0; j < n; j++){
+//             cout<<ch;
+//             ch += 1; 
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 3:- 1 2 3
+        //  4 5 6
+        //  7 8 9
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<< "Enter the number: ";
+//     cin>>n;
+//     int num = 1;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < n; j++){
+//          cout<<num;
+//          num+=1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }      
+
+// ques 4:- A B C
+        //  D E F
+        //  G H I
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<< "Enter the number: ";
+//     cin>>n;
+//     char ch = 'A';
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < n; j++){
+//           cout<<ch;
+//           ch++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// } 
+
+// ques 5:-
+//    *
+//    * *
+//    * * *
+//    * * * *
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < i+1; j++){
+//             cout<<"*" << " ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 6:-
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < i+1; j++){
+//             cout<<(i+1)<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 7:-
+// A
+// B B
+// C C C
+// D D D D
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < (i+1); j++){
+//            cout<< char(65+i)<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 8:-
+// 1 
+// 1 2 
+// 1 2 3
+// 1 2 3 4
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         int num = 1;
+//         for(int j = 0; j < (i+1); j++){
+//           cout<<num<<" ";
+//           num++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
