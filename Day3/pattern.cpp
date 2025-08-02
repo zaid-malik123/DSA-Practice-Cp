3cc9d6c2bcf3a249564d292de4ecdc94e8a6c3f0
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-02 18:24:22 +0530

    pattern-done

--- Day3/pattern.cpp
+++ Day3/pattern.cpp
@@ -209,3 +209,171 @@
 //     }
 //     return 0;
 // }
+
+// ques 11:-
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int num = 1;
+//     for(int i = 0; i < n; i++){
+//       for(int j = 0; j < (i+1); j++){
+//         cout<<num<< " ";
+//         num++;
+//       }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 12:-
+// A
+// B C
+// D E F
+// G H I J
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int num = 65;
+//     for(int i = 0; i < n; i++){
+//       for(int j = 0; j < (i+1); j++){
+//         cout<<char(num)<< " ";
+//         num++;
+//       }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ques 13:-
+// 1 1 1 1
+//   2 2 2
+//     3 3
+//       4
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//       for(int j = 0; j < i; j++){
+//         cout<<" " ;
+//       }
+//       for(int j = 0; j < n-i; j++){
+//         cout<< (i+1);
+//       }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// practice:- 
+//      * 
+//     * * *
+//   * * * * *
+// * * * * * * *
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"Enter the number: ";
+// int str = 1;
+// cin>>n;
+// for(int i = 0; i < n; i++){
+//       for(int j = 0; j < n-i-1; j++){
+//            cout<<" "<< " ";
+//       }
+//       for(int k = 0; k < str; k++){
+//         cout<<"*"<<" ";
+//       }  
+//       cout<<endl;
+//       str += 2;
+// }
+
+// return 0;
+// }
+
+// ques 14:-
+//          1
+//       1  2  1
+//    1  2  3  2  1
+// 1  2  3  4  3  2  1
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "Enter the number: ";
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++){
+//         for(int space = 0; space < n - i - 1; space++){
+//             cout << "  "; 
+//         }
+//         for(int num = 1; num <= i + 1; num++){
+//             cout << num << " ";
+//         }
+//         for(int num = i; num >= 1; num--){
+//             cout << num << " ";
+//         }
+
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+// ques 15:-
+// * - - - - * 
+// * * - - * *
+// * * * * * *
+// * * * * * *
+// * * - - * *
+// * - - - - *
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"Enter the number: ";
+// cin>>n;
+// // upper butterfly
+// for(int i = 0; i < n; i++){
+//         for(int j = 0; j < i+1; j++){
+//                 cout<<"*"<<" ";
+//         }
+//        for(int j = 0; j< 2*(n-i-1); j++){
+//         cout<< "- ";
+//        }
+//        for(int l = 0; l < i+1; l++){
+//           cout<<"*"<<" ";
+//        }
+//         cout<<endl;
+// }
+// // lower butterfly
+// for(int i = 0; i < n; i++){
+//         for(int j = 0; j < n-i; j++){
+//                 cout<<"*"<<" ";
+//         }
+//        for(int j = 0; j< 2*i; j++){
+//         cout<< "- ";
+//        }
+//        for(int j = 0; j < n-i; j++){
+//                 cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+// }
+
+// return 0;
+// }
