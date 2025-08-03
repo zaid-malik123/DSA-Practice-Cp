a1eae3bbca528b3261aa01806e02d2bd1b3bf7aa
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-04 00:13:17 +0530

    solver-pattern-question

--- Day3/pattern.cpp
+++ Day3/pattern.cpp
@@ -379,6 +379,7 @@
 // }
 
 // practice:- 
+
 // ---* 
 // --*** 
 // -***** 
@@ -415,4 +416,31 @@
  
 
 //  return 0;
+// }
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < i+1; j++){
+//                 cout<<"*"<<" ";
+//         }
+//         for(int k = 0; k < 2*(n-i-1); k++){
+//                 cout<<" "<<" ";
+//         }
+//          for(int l = 0; l < i+1; l++){
+//                 cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }    
+//     return 0;    
 // }
