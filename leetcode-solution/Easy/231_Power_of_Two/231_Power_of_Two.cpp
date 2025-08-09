dbcf3e09c2497a068f3b3809dd281f7f5ec26859
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-09 08:55:07 +0530

    update-readme

--- leetcode-solution/Easy/231_Power_of_Two/231_Power_of_Two.cpp
+++ leetcode-solution/Easy/231_Power_of_Two/231_Power_of_Two.cpp
@@ -1,28 +1,47 @@
+// #include<iostream>
+// using namespace std;
+
+// bool checkPowOfTwo(int n){
+//     if(n <= 0){ 
+//         return false;
+//     }
+//     while (n != 1){
+//         if(n % 2 == 1){
+//             return false;
+//         }
+//         n = n / 2;
+//     }
+//     return true;
+// }
+
+// int main(){
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
+//     if(checkPowOfTwo(n)){
+//         cout << n << " is a power of 2" << endl;
+//     } else {
+//         cout << n << " is NOT a power of 2" << endl;
+//     }
+
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
-
-bool checkPowOfTwo(int n){
-    if(n <= 0){ 
-        return false;
-    }
-    while (n != 1){
-        if(n % 2 == 1){
-            return false;
-        }
-        n = n / 2;
-    }
+bool checkPow2(int n){
+ if(n < 0){
+    return false;
+ }
+ if(n == 1){
     return true;
+ }
 }
-
 int main(){
     int n;
-    cout << "Enter the number : ";
-    cin >> n;
-    if(checkPowOfTwo(n)){
-        cout << n << " is a power of 2" << endl;
-    } else {
-        cout << n << " is NOT a power of 2" << endl;
-    }
-
+    cout<<"Enter the number : ";
+    cin>>n;
+    cout<<checkPow2(n);
     return 0;
 }
---
+++ leetcode-solution/README.md
@@ -0,0 +1,29 @@
+# 📘 LeetCode Solutions by Zaid Malik
+
+This repository contains my solutions to LeetCode problems in C++. I solve problems regularly to improve my problem-solving and data structure knowledge.
+
+---
+
+## 📊 Progress
+
+| Difficulty | Solved |
+|------------|--------|
+| Easy       | 06     |
+| Medium     | 0     |
+| Hard       | 0    |
+
+---
+
+## 🗂️ Folder Structure
+
+- `Easy/` – Easy problems
+- `Medium/` – Medium problems
+- `Hard/` – Hard problems
+- `Daily-Challenge/` – LeetCode Daily Problems
+- `Notes/` – My DSA patterns, tips, and tricks
+
+---
+
+## 🧠 Topics Covered
+
+- Arrays, Strings, HashMap, Sliding Window, Recursion, DP, Greedy...
