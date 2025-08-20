b975ffccf53a915c30c71162bc88ed1f702b62ca
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-20 23:06:51 +0530

    .

--- 06_Arrays/LinearSearch/LinearSearch.cpp
+++ 06_Arrays/LinearSearch/LinearSearch.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int linearSearch(vector <int> &v, int target){
-for(int i = 0; i < v.size(); i++){
-    if(v[i] == target){
-        return i;
-        break;
-    }
-}
-return -1;
-}
-int main(){
-    vector <int> v = {1, 2, 3, 4, 5};
-    int target = 3;
-    int idx = linearSearch(v, target);
-    if(idx != -1){
-        cout<<"idx found at"<< idx<<endl;
-    }
-    else {
-        cout<<"idx not found";
-    }
-    return 0;
-}
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int linearSearch(vector <int> &v, int target){
+// for(int i = 0; i < v.size(); i++){
+//     if(v[i] == target){
+//         return i;
+//         break;
+//     }
+// }
+// return -1;
+// }
+// int main(){
+//     vector <int> v = {1, 2, 3, 4, 5};
+//     int target = 3;
+//     int idx = linearSearch(v, target);
+//     if(idx != -1){
+//         cout<<"idx found at"<< idx<<endl;
+//     }
+//     else {
+//         cout<<"idx not found";
+//     }
+//     return 0;
+// }
