ff38f86a55490368d6e740d91abb646ae0b14d93
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-20 23:11:01 +0530

    git push

--- 02_Conditionals_Loops/Loop.cpp
+++ 02_Conditionals_Loops/Loop.cpp
@@ -13,6 +13,15 @@
 //     return 0;
 // }
 
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 2; i < 21; i= i+2 ){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
 // #include<iostream>
 // using namespace std;
 // int main(){
