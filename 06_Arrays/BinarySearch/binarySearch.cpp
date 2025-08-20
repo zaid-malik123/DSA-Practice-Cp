ee2eb993634803cea0f81648c0a4cea56c224bd9
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-20 23:08:30 +0530

    .

--- 06_Arrays/BinarySearch/binarySearch.cpp
+++ 06_Arrays/BinarySearch/binarySearch.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int binarySearch(vector<int> &v, int target)
-{
-    int s = 0;
-    int e = v.size() - 1;
-    while (s <= e)
-    {
-        int mid = s + (e - s) / 2;
-        if (v[mid] == target)
-        {
-            return mid;
-        }
-        else if (v[mid] > target)
-        {
-            e = mid - 1;
-        }
-        else
-        {
-            s = mid + 1;
-        }
-    }
-    return -1;
-}
-int main()
-{
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int target = 9;
-    int idx = binarySearch(v, target);
-    if (idx != -1)
-    {
-        cout << "Element found at index" << idx << endl;
-    }
-    else
-    {
-        cout << "Element not found";
-    }
-}
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int binarySearch(vector<int> &v, int target)
+// {
+//     int s = 0;
+//     int e = v.size() - 1;
+//     while (s <= e)
+//     {
+//         int mid = s + (e - s) / 2;
+//         if (v[mid] == target)
+//         {
+//             return mid;
+//         }
+//         else if (v[mid] > target)
+//         {
+//             e = mid - 1;
+//         }
+//         else
+//         {
+//             s = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+// int main()
+// {
+//     vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int target = 9;
+//     int idx = binarySearch(v, target);
+//     if (idx != -1)
+//     {
+//         cout << "Element found at index" << idx << endl;
+//     }
+//     else
+//     {
+//         cout << "Element not found";
+//     }
+// }
--- 06_Arrays/BubleSort/bubleSort.cpp
+++ 06_Arrays/BubleSort/bubleSort.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr)/ sizeof(arr[0]);
-    for(int i = 0; i < n-1; i++){
-        for(int j = 0; j < n-i-1; j++){
-            swap(arr[j], arr[j+1]);
-        }
-    }
-    for(int i = 0; i < n; i++){
-        cout<< arr[i]<<" ";
-    }
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(arr)/ sizeof(arr[0]);
+//     for(int i = 0; i < n-1; i++){
+//         for(int j = 0; j < n-i-1; j++){
+//             swap(arr[j], arr[j+1]);
+//         }
+//     }
+//     for(int i = 0; i < n; i++){
+//         cout<< arr[i]<<" ";
+//     }
+// }
