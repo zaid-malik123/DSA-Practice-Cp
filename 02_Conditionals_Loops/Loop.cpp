afc0829402c74bd66664f9a16caf03a8cc7e81e1
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-26 22:37:02 +0530

    practice

--- 02_Conditionals_Loops/Loop.cpp
+++ 02_Conditionals_Loops/Loop.cpp
@@ -13,14 +13,14 @@
 //     return 0;
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    for(int i = 2; i < 21; i= i+2 ){
-        cout<<i<<" ";
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i = 2; i < 21; i= i+2 ){
+//         cout<<i<<" ";
+//     }
+//     return 0;
+// }
 
 // #include<iostream>
 // using namespace std;
