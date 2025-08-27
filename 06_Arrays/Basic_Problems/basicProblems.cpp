ca4ed5df55510cebc33d1b606de1257b1737b624
Zaid Malik <mzaidmalik25@gmail.com>
2025-08-27 21:24:57 +0530

    practice

--- 06_Arrays/Basic_Problems/basicProblems.cpp
+++ 06_Arrays/Basic_Problems/basicProblems.cpp
@@ -170,22 +170,22 @@
 //     return 0;
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[6] = {1, 3, 5, 6, 9, 10};
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[6] = {1, 3, 5, 6, 9, 10};
 
-   for(int i = 0; i < 6; i++){
-        for(int j = i + 1; j < 6; j++){
-            if(arr[i] > arr[j]){
-                swap(arr[i], arr[j]);
-            }
-        }
-    }
+//    for(int i = 0; i < 6; i++){
+//         for(int j = i + 1; j < 6; j++){
+//             if(arr[i] > arr[j]){
+//                 swap(arr[i], arr[j]);
+//             }
+//         }
+//     }
 
 
-    for(int i = 0; i < 6; i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
-}
+//     for(int i = 0; i < 6; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
