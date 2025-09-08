909dc082417e89ca0a52c9ff728e89000de41843
Zaid Malik <mzaidmalik25@gmail.com>
2025-09-08 20:23:33 +0530

    reverse number using for loop

--- 02_Conditionals_Loops/Loop.cpp
+++ 02_Conditionals_Loops/Loop.cpp
@@ -147,3 +147,21 @@
 //     cout << "Factorial of " << n << " is " << fact;
 //     return 0;
 // }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     int rev = 0;
+//     while(n > 0){
+//         int digit = n % 10;
+//         rev = rev * 10 + digit;
+//         n = n / 10;
+//     }
+
+//     cout << "Reversed number is: " << rev;
+//     return 0;
+// }
